aa25fa2e5219e906c4cb0e9d9ad4a5c820d743cb
ooghenekaro <114274865+ooghenekaro@users.noreply.github.com>
2023-05-21 11:14:43 +0200

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -62,7 +62,7 @@ function Cart() {
         </div>
         {basket.length === 0 ? (
           <div>
-            <h2>Your basket is not empty, Oops !</h2>
+            <h2>Your basket is not empty,  Oopsy !</h2>
             <h6>Start Adding Items to your basket</h6>
           </div>
         ) : (
